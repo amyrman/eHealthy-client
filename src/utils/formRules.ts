0b4a8a975c635bae1838a67b683275dfba05c199
Anders Myrman <amyhrman@gmail.com>
2024-04-15 22:17:16 +0200

    Add validation rules

--- src/utils/formRules.ts
+++ src/utils/formRules.ts
@@ -1,10 +1,17 @@
 export const emailRules = [
-  (value: string) => value ? true : 'You must enter an email address.',
-  (value: string) => /.+@.+\..+/.test(value) ? true : 'You must enter a valid email address.'
+  (value: string) => (value ? true : 'You must enter an email address.'),
+  (value: string) => (/.+@.+\..+/.test(value) ? true : 'You must enter a valid email address.')
 ]
 
 export const passwordRules = [
-  (value: string) => value ? true : 'Password is required.',
-      (value: string) =>
-        (value && value.length >= 8) ? true : 'Password must be at least 8 characters long.'
+  (value: string) => (value ? true : 'Password is required.'),
+  (value: string) =>
+    value && value.length >= 8 ? true : 'Password must be at least 8 characters long.'
+]
+export const firstRules = [(value: string) => (value ? true : 'First name is required.')]
+
+export const lastRules = [(value: string) => (value ? true : 'Last name is required.')]
+
+export const termsRules = [
+  (value: boolean) => (value ? true : 'You must accept the terms and conditions.')
 ]
