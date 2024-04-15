e4c7afe19110bb021799a91b73ccdce57b2402b2
Anders Myrman <amyhrman@gmail.com>
2024-04-15 21:30:10 +0200

    Separate validation rules

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,17 +1,11 @@
 <script lang="ts">
+import { emailRules, passwordRules } from '../utils/formRules';
 export default {
   data: () => ({
     email: null,
     password: null,
-    emailRules: [
-      (value: string) => (value ? true : 'You must enter an email address.'),
-      (value: string) => (/.+@.+\..+/.test(value) ? true : 'You must enter a valid email address.')
-    ],
-    passwordRules: [
-      (value: string) => (value ? true : 'Password is required.'),
-      (value: string) =>
-        value && value.length >= 8 ? true : 'Password must be at least 8 characters long.'
-    ]
+    emailRules,
+    passwordRules
   })
 }
 </script>
---
+++ src/utils/formRules.ts
@@ -0,0 +1,10 @@
+export const emailRules = [
+  (value: string) => value ? true : 'You must enter an email address.',
+  (value: string) => /.+@.+\..+/.test(value) ? true : 'You must enter a valid email address.'
+]
+
+export const passwordRules = [
+  (value: string) => value ? true : 'Password is required.',
+      (value: string) =>
+        value && value.length >= 8 ? true : 'Password must be at least 8 characters long.'
+]
