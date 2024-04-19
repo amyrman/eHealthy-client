ee8db7df2c35c52dde3c06c6b436c13087409890
Anders Myrman <amyhrman@gmail.com>
2024-04-19 04:42:29 +0200

    set baseURL

--- src/http-common.ts
+++ src/http-common.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 export default axios.create({
-  baseURL: 'http://your-backend-url', // TODO: change to real url
+  baseURL: 'http://localhost:8080/server-1.0-SNAPSHOT/api/',
   headers: {
     'Content-type': 'application/json'
   }
