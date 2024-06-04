5b523a7fb41abae48073d3cade4f7141531db309
Anders Myrman <amyhrman@gmail.com>
2024-06-04 15:36:52 +0200

    refactor tsconfigs

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,26 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
+import { configDefaults } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    VueDevTools(),
-  ],
+  plugins: [vue(), VueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    exclude: [...configDefaults.exclude, 'e2e/**'],
+    root: fileURLToPath(new URL('./', import.meta.url)),
+    server: {
+      deps: {
+        inline: ['vuetify']
+      }
     }
   }
 })
