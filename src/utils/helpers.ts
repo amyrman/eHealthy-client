46ea3246ef6e20613172f664cc396c5991080fbc
Anders Myrman <amyhrman@gmail.com>
2024-06-04 14:39:45 +0200

    Improve readability, typing

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -3,24 +3,20 @@
  * @param status - The HTTP status code.
  * @returns The error message corresponding to the status code.
  */
-export function handleStatusCode(status: number) {
-  switch (status) {
-    case 400:
-    return "There seems to be a problem with your request. Please check your details and try again.";
-    case 401:
-    return "Your login details are incorrect. Please try again.";
-    case 403:
-    return "You don't have permission to do this.";
-    case 404:
-    return "We couldn't find what you were looking for.";
-    case 429:
-    return "You've been making a lot of requests. Please wait a bit and try again.";
-    case 500:
-    case 502:
-    case 503:
-    case 504:
-    return "Something went wrong on our end. Please try again later.";
-    default:
-    return "Something went wrong. Please try again.";
-  }
+type HttpStatusCode = 400 | 401 | 403 | 404 | 429 | 500 | 502 | 503 | 504;
+
+export function handleStatusCode(status: number): string {
+  const statusMessages: { [key in HttpStatusCode]: string } = {
+    400: "There seems to be a problem with your request. Please check your details and try again.",
+    401: "Your login details are incorrect. Please try again.",
+    403: "You don't have permission to do this.",
+    404: "We couldn't find what you were looking for.",
+    429: "You've been making a lot of requests. Please wait a bit and try again.",
+    500: "Something went wrong on our end. Please try again later.",
+    502: "Something went wrong on our end. Please try again later.",
+    503: "Something went wrong on our end. Please try again later.",
+    504: "Something went wrong on our end. Please try again later.",
+  };
+
+  return statusMessages[status as HttpStatusCode] || "Something went wrong. Please try again.";
 }
